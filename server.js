7da2b3dac91bb85ae801a558ebd017eddacac1a7
Jigar Donga <69411775+jigardonga105@users.noreply.github.com>
2025-10-25 15:30:27 -0400

    change

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const Emitter = require('events')
 const multer = require("multer");
 
 let dbUrl = process.env.MONGO_CONNECTION_URL;
-console.log("DB URL:", dbUrl);
 require('./app/helper/dbConn')
 
 //Event Emitter
